01ddb02c2600e1cd98d46ac775b729e23c86b6d4
Chris Roos <chris@seagul.co.uk>
2015-05-03 20:42:00 +0100

    Replace FileWatcher with a loop and custom cache
    I was having problems with FileWatcher detecting partially copied files. I
    considered using FileWatcher to build a list of files to check and then to
    check them separately but that sounded like it might end up being more
    complicated than necessary.
    
    I've ended up recording a digest of each of the files in the tracks directory.
    I only try to process a file if I haven't seen it before (i.e. the digest file
    doesn't exist).
    
    I've added a little more info to my gist -
    https://gist.github.com/chrisroos/867d3696278f616cb734

--- monitor.rb
+++ monitor.rb
@@ -1,17 +1,36 @@
 require File.expand_path('../config/boot.rb', __FILE__)
-require 'filewatcher'
+require 'digest'
+require 'fileutils'
 require 'track_creator'
+
+DIGEST_DIRECTORY = File.expand_path('../tmp/digests', __FILE__)
+FileUtils.mkdir_p(DIGEST_DIRECTORY)
 
 LOGGER.info "Monitoring #{TRACK_DIRECTORY} for new tracks"
 
-FileWatcher.new(File.join(TRACK_DIRECTORY, '**/*.mp3')).watch do |filename|
-  mp3_path = File.expand_path(filename)
-  next unless File.exists?(mp3_path)
+while true
+  LOGGER.info 'Checking for new/changed files'
 
-  begin
-    TrackCreator.create_or_update_for(mp3_path)
-  rescue ActiveRecord::RecordInvalid
-    LOGGER.info "Artist not found in #{mp3_path}"
-    LOGGER.info "Skipping in the hope that it's caused by reading a partially copied file"
+  Dir[File.join(TRACK_DIRECTORY, '**/*.mp3')].each do |filename|
+    mp3_path = File.expand_path(filename)
+
+    digest = Digest::MD5.file(mp3_path).hexdigest
+    digest_path = File.join(DIGEST_DIRECTORY, digest)
+
+    unless File.exists?(digest_path)
+      LOGGER.info "New or changed file: #{File.basename(mp3_path)}"
+      FileUtils.touch(digest_path)
+
+      begin
+        TrackCreator.create_or_update_for(mp3_path)
+      rescue ActiveRecord::RecordInvalid
+        LOGGER.info "Artist not found in #{mp3_path}"
+        LOGGER.info "Skipping in the hope that it's caused by reading a partially copied file"
+      end
+    end
   end
+
+  LOGGER.info 'Finished checking'
+
+  sleep 30
 end
