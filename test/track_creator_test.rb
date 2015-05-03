62dbb1afb80d7f57c5b14d1d5a11aebc19879a46
Chris Roos <chris@seagul.co.uk>
2015-05-03 15:46:45 +0100

    Improve integration test
    Programmatically setting the ID3 tags makes the test easier to understand.
    
    I copy the original test.mp3 file, set the tags and then remove it once I'm
    done. This avoids making changes to the original test.mp3 file.

--- test/track_creator_test.rb
+++ test/track_creator_test.rb
@@ -8,38 +8,53 @@ config = YAML.load(File.read(config_filename))
 connection_settings = config[:database][:test]
 ActiveRecord::Base.establish_connection(connection_settings)
 
-<<-EndReadme
-The test.mp3 was downloaded from http://www.xamuel.com/blank-mp3s/
-and edited using id3tag from id3lib
-
-id3tag test/fixtures/test.mp3 \
- --artist="artist-string" \
- --album="album-string" \
- --song="song-name-string" \
- --year="2015" \
- --track="03"
-
- NOTE. I tried to set the genre but whatever I try it comes out as Blues so that's what I'm testing below.
-EndReadme
-
 class TrackCreatorIntegrationTest < Test::Unit::TestCase
+  def setup
+    FileUtils.cp original_mp3_path, temporary_mp3_path
+
+    TagLib::MPEG::File.open(temporary_mp3_path) do |file|
+      tag = file.tag
+      tag.artist = 'artist-string'
+      tag.album = 'album-string'
+      tag.title = 'song-name-string'
+      tag.year = 2015
+      tag.track = 3
+      tag.genre = 'Blues'
+      file.save
+    end
+  end
+
+  def teardown
+    FileUtils.rm temporary_mp3_path
+  end
+
   def test_should_create_new_artist_and_track
     Artist.delete_all
     Track.delete_all
 
-    mp3_path = File.expand_path('../fixtures/test.mp3', __FILE__)
-    TrackCreator.create_or_update_for(mp3_path)
+    TrackCreator.create_or_update_for(temporary_mp3_path)
 
     assert_equal 1, Artist.count
     assert_equal 1, Artist.first.tracks.length
     assert_equal 'artist-string', Artist.first.name
 
     assert_equal 1, Track.count
-    assert_equal 'test.mp3', Track.first.filename
+    assert_equal 'test.temporary.mp3', Track.first.filename
     assert_equal 'album-string', Track.first.album
     assert_equal 'song-name-string', Track.first.title
     assert_equal 2015, Track.first.year
     assert_equal 3, Track.first.track_number
     assert_equal 'Blues', Track.first.genre
+  end
+
+  private
+
+  def temporary_mp3_path
+    File.expand_path('../fixtures/test.temporary.mp3', __FILE__)
+  end
+
+  def original_mp3_path
+    # The test.mp3 was downloaded from http://www.xamuel.com/blank-mp3s/
+    File.expand_path('../fixtures/test.mp3', __FILE__)
   end
 end
